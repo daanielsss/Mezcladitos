56366a258ddd93121ab3cce63f496881a09768a6
daanielsss <danielameric2112o@gmail.com>
2025-11-19 21:02:13 -0600

    Estructura 8.0

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import App from './App' // <-- Importamos la App
+import { HashRouter } from 'react-router-dom' // <-- Importamos el Router
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>,
 )
 
